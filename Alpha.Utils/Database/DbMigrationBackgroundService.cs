f731507932aa3e2f87305751b3c9eeefed321046
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-03-04 20:17:36 -0300

    Create Alpha Utils

---
+++ Alpha.Utils/Database/DbMigrationBackgroundService.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Alpha.Utils.Database;
+
+public class DbMigrationBackgroundService<T>(IServiceProvider serviceProvider) : BackgroundService
+    where T : DbContext
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var scope = serviceProvider.CreateScope();
+        var dataContext = scope.ServiceProvider.GetRequiredService<T>();
+
+        dataContext.Database.Migrate();
+
+        return Task.CompletedTask;
+    }
+}
