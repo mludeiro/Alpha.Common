c9a5b7e04de48e5ad3a7e754c05195f199829837
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-03-06 19:06:07 -0300

    Adding Consul service extension

---
+++ Alpha.Utils/Consul/ConsulServicesExtensions.cs
@@ -0,0 +1,20 @@
+using Consul;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Alpha.Utils.Consul;
+
+public static class ConsulServicesExtensions
+{
+    public static IServiceCollection ConsulServicesConfig(this IServiceCollection services, ConsulConfig consulConfig )
+    {
+        services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(config =>
+        {
+            config.Address = new Uri(consulConfig.ConsulAddress!);
+        }));
+
+        services.AddSingleton<IHostedService, ConsulHostedService>();
+        services.AddSingleton(consulConfig);
+        return services;
+    }
+}
