34b875937740443dd8e650b08a5e174cd0a6fce9
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-03-08 08:50:07 -0300

    Fixing unsuscribe consul errors

--- Alpha.Utils/Consul/ConsulRegistryService.cs
+++ Alpha.Utils/Consul/ConsulRegistryService.cs
@@ -10,6 +10,25 @@ public class ConsulHostedService(IConsulClient consulClient, ConsulConfig consul
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
+        AgentServiceRegistration registration = CreateRegistration();
+
+        logger.LogInformation($"Registering service with Consul: {registration.Name}");
+
+        await consulClient.Agent.ServiceDeregister(registration.ID, cancellationToken);
+        await consulClient.Agent.ServiceRegister(registration, cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Deregistering service from Consul: {consulConfig.ServiceId}");
+
+        AgentServiceRegistration registration = CreateRegistration();
+
+        await consulClient.Agent.ServiceDeregister(registration.ID, cancellationToken);
+    }
+
+    private AgentServiceRegistration CreateRegistration()
+    {
         var host = Dns.GetHostName();
         var stringPort = Environment.GetEnvironmentVariable("ASPNETCORE_HTTP_PORTS");
         var port = stringPort is null ? 8080 : int.Parse(stringPort);
@@ -31,19 +50,6 @@ public class ConsulHostedService(IConsulClient consulClient, ConsulConfig consul
         };
 
         registration.Checks = [check];
-
-        logger.LogInformation($"Registering service with Consul: {registration.Name}");
-
-        await consulClient.Agent.ServiceDeregister(registration.ID, cancellationToken);
-        await consulClient.Agent.ServiceRegister(registration, cancellationToken);
-    }
-
-    public async Task StopAsync(CancellationToken cancellationToken)
-    {
-        logger.LogInformation($"Deregistering service from Consul: {consulConfig.ServiceId}");
-
-        var registration = new AgentServiceRegistration { ID = consulConfig.ServiceId };
-
-        await consulClient.Agent.ServiceDeregister(registration.ID, cancellationToken);
+        return registration;
     }
 }
