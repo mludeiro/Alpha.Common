f00b6784eb2e31cc7c2e3520a8b2b567ad88f8f0
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-03-14 11:58:01 -0300

    Update Token interface

--- Alpha.Common/TokenService/ITokenService.cs
+++ Alpha.Common/TokenService/ITokenService.cs
@@ -5,7 +5,7 @@ namespace Alpha.Common.TokenService;
 public interface ITokenService
 {
     [Post("/api/token")]
-    public Task<ApiResponse<TokenGeneration?>> GetToken([Body]List<ClaimValue> claimValues);
+    public Task<ApiResponse<TokenGeneration?>> GetToken([Body]TokenRequest tokenRequest);
 
     [Get("/api/check")]
     public Task<ApiResponse<object>> CheckToken([Header("Authorization")] string authorization);
